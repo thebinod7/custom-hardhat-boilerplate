ad24284b55380a086e27c87fd2a00732c59d3e8c
Binod Chaudhary <thebinod7@gmail.com>
2024-12-03 07:13:37 +0545

    cleanup

--- frontend/app/lib/config.ts
+++ frontend/app/lib/config.ts
@@ -5,22 +5,28 @@ import { mainnet, sepolia, bscTestnet, base } from "wagmi/chains";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 const PROJECT_ID = process.env.NEXT_PUBLIC_RAINBOW_PROJECT_ID as string;
-
-const supportedChains = [mainnet, sepolia, bscTestnet, base];
+const SUPPORTED_CHAINS = [mainnet, sepolia, bscTestnet, base];
 
 export const config = getDefaultConfig({
   appName: "WalletConnection",
   projectId: PROJECT_ID,
-  chains: supportedChains as any,
+  chains: SUPPORTED_CHAINS as any,
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
   }),
-  transports: supportedChains.reduce(
+  transports: SUPPORTED_CHAINS.reduce(
     (obj, chain) => ({ ...obj, [chain.id]: http() }),
     {}
   ),
 });
+
+// ========= Transports reduce to =========
+// transports: {
+//   1: http(), // Ethereum
+//   137: http(), // Polygon
+//   56: http() // Binance Smart Chain
+// }
 
 // export const config = getDefaultConfig({
 //   appName: "My RainbowKit App",
